bda27f7479a4b17605c68519df08b05c5b7193f1
Jacob Young <jacob@jry.io>
2016-09-24 18:29:43 -0400

    Initial Commit
    Add first iteration of commit hook. Uses ip-api.com/json to fetch
    geolocation data assuming the user has an internet connection.
    
    It parses the relevant data fields and writes to a csv file in the home
    directory (~/.git-commit-geolocate/locations.csv)

---
+++ post-commit.sh
@@ -0,0 +1,73 @@
+# !/bin/bash
+#
+# Copyright Jacob Young - 2016
+# License MIT
+
+set -o nounset
+set -o errexit
+
+# Make a curl request to ip-api
+# Handle api error (no internet)
+
+# Data store directory
+DATA_STORE_DIR=".git-commit-geolocate"
+
+result=$( curl -s -X GET "http://ip-api.com/json" )
+
+if [ $? -eq 1 ]; then
+	echo "Git Locate: FAILED - Geolocation failed. Could not save geolocation. Either ip-api.com/json is down, or your internet connection is faulty."
+  exit 0
+fi
+
+if ! type pcregrep > /dev/null ; then
+  echo "Git Locate: FAILED - Missing pcregrep."
+  echo "    Install pcregrep on OS X using \`brew install pcre\`"
+fi
+
+# Record the timestamp of the git commit
+# TODO: Use first argument passed into script to parse git commit time
+timestamp=$( date +"%s" )
+
+# Repostory name:
+# ref: http://stackoverflow.com/questions/15715825/how-do-you-get-git-repos-name-in-some-git-repository
+#   The git rev-parse --show-toplevel part gives you the path to that directory
+#   and the basename strips the first part of the path.
+repo_path=$( git rev-parse --show-toplevel )
+repo_base=$( basename $repo_path )
+
+echo "Repo name: $repo_path"
+
+cd $repo_path
+
+# Commit sha
+commit_sha=$( git rev-parse --verify HEAD )
+
+# Commit message
+commit_msg=$( git show -s --format=%s $commit_sha )
+
+# Parse the result for latitude and longitude
+lat=$( echo "$result" | pcregrep -o '"lat":(\-?\d*?,|.*?[^\\]"?,)' | pcregrep -o "\-?\d.*[^,]")
+lon=$( echo "$result" | pcregrep -o '"lon":(\-?\d*?,|.*?[^\\]"?,)' | pcregrep -o "\-?\d.*[^,]")
+
+# Log variables
+echo "Timestamp: $timestamp"
+echo "Repostory naem: $repo_path"
+echo "Commit Sha: $commit_sha"
+echo "Commit Message: $commit_msg"
+echo "Latitude: $lat"
+echo "Longitude: $lon"
+
+cd ~
+mkdir -p $DATA_STORE_DIR
+cd $DATA_STORE_DIR
+
+# Check if the CSV data store exists
+if [ ! -f $DATA_STORE_DIR/locations.csv ]; then
+  echo "Git Locate: Could not file file locations.csv. Making a new file"
+  echo "time, repo, sha, msg, lat, lon" >> locations.csv
+fi
+
+# Write to CSV
+echo "$timestamp, $repo_base, $commit_sha, $commit_msg, $lat, $lon" >> locations.csv
+echo "Geo Locate: Location recorded!"
+
